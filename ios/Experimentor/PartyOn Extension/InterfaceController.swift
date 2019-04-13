44aea8ec8b53ff9698f115a9c3c8ef270828fa27
Nataliya Patsovska <nataliya.patsovska@izettle.com>
2019-04-13 12:47:23 +0200

    Change a label once the workout session is connected

--- ios/Experimentor/PartyOn Extension/InterfaceController.swift
+++ ios/Experimentor/PartyOn Extension/InterfaceController.swift
@@ -8,24 +8,54 @@
 
 import WatchKit
 import Foundation
+import HealthKit
 
+class InterfaceController: WKInterfaceController, HKWorkoutSessionDelegate {
 
-class InterfaceController: WKInterfaceController {
+    @IBOutlet weak var textLabel: WKInterfaceLabel!
+
+    let healthStore = HKHealthStore()
+    var session: HKWorkoutSession!
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
+
+        let configuration = HKWorkoutConfiguration()
+        configuration.activityType = .running
+        configuration.locationType = .indoor
+
+        do {
+            session = try HKWorkoutSession(healthStore: healthStore, configuration: configuration)
+
+            session.delegate = self
+            session.prepare()
+        }
+        catch let error as NSError {
+            // Perform proper error handling here...
+            fatalError("*** Unable to create the workout session: \(error.localizedDescription) ***")
+        }
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        session.startActivity(with: Date())
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+
+
+    func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
+        print("workoutSessionDidChangeTo \(toState)")
+        textLabel.setText("Connected")
+    }
+
+    func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
+        print("workoutSessionDidFailWithError \(error)")
+        textLabel.setText("Error")
     }
 
 }

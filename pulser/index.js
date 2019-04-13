dae67b519c512f91eede9f76c9bcb8262f4ee929
Mattias Jähnke <mattias.jahnke@STKIZ00390.local>
2019-04-13 12:48:22 +0200

    ficxex

--- pulser/index.js
+++ pulser/index.js
@@ -1,9 +1,11 @@
 const SerialPort = require('serialport')
+const fetch = require('node-fetch')
 const port = new SerialPort('/dev/cu.usbserial-14220', {
   baudRate: 9600
 })
 
 var heartbeat_bpm = 65
+var pulseInterval;
 
 const sendPulse = () => {
     port.write('0', function(err) {
@@ -11,8 +13,27 @@ const sendPulse = () => {
     })
 }
 
+const setupPulseTimer = () => {
+    if (pulseInterval !== undefined) {
+        clearInterval(pulseInterval);
+    }
+    pulseInterval = setInterval(sendPulse, bpmToMs(heartbeat_bpm));
+}
+
+const fetchHeartbeatBpm = () => {
+    fetch('https://hackfest-jaeger.herokuapp.com/')
+    .then(res => res.json())
+    .then(json => {
+        const hb = parseInt(json.beat);
+        console.log("Fetched new heartbeat: " + hb);
+        heartbeat_bpm = hb;
+        setupPulseTimer();
+    });
+}
+
 const bpmToMs = (bpm) => {
     return 60000 / bpm
 }
 
-setInterval(sendPulse, bpmToMs(heartbeat_bpm));
+setInterval(fetchHeartbeatBpm, 10 * 1000);
+setupPulseTimer();

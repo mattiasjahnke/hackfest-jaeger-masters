8ec4c41c7555cfbab4d4326797a53e471cc47f8c
Nataliya Patsovska <nataliya.patsovska@izettle.com>
2019-04-13 10:26:06 +0200

    try to get some hearth rate samples

--- ios/Experimentor/Experimentor/ViewController.swift
+++ ios/Experimentor/Experimentor/ViewController.swift
@@ -10,34 +10,85 @@ import UIKit
 import HealthKit
 
 class HealthKitManager: NSObject {
-    static let healthKitStore = HKHealthStore()
+
+    var hearthRateType: HKSampleType {
+        guard let heartRate = HKSampleType.quantityType(forIdentifier: .heartRate) else {
+                fatalError("can't create heartRate type")
+        }
+        return heartRate
+    }
+
+    var energyBurnedType: HKSampleType {
+        guard let energyBurned = HKSampleType.quantityType(forIdentifier: .activeEnergyBurned) else {
+            fatalError("can't create energyBurnedType type")
+        }
+        return energyBurned
+    }
+
+    var healthKitTypes: Set<HKSampleType> {
+        return [hearthRateType, energyBurnedType]
+    }
+
+    let healthKitStore = HKHealthStore()
     
-    static func authorizeHealthKit() {
+    func authorizeHealthKit() {
         guard HKHealthStore.isHealthDataAvailable() else {
             fatalError("Health data not available")
         }
-
-        guard
-            let quantityType = HKSampleType.quantityType(forIdentifier: .heartRate),
-            let energyBurned = HKSampleType.quantityType(forIdentifier: .activeEnergyBurned)
-        else { fatalError("can't create quantity type") }
-
-        
-        let healthKitTypes: Set<HKSampleType> = [ quantityType, energyBurned ]
         
         healthKitStore.requestAuthorization(toShare: [], read: healthKitTypes) { (granted, error) in
             print("Granted? \n\(granted ? "hell yeah" : "nope")")
+            if granted && error == nil {
+                self.startMonitoringHearthRate()
+            }
         }
     }
     
-    func startMonitoring() {
-        guard let sampleType = HKSampleType.quantityType(forIdentifier: .heartRate) else { fatalError() }
-        
-        let query = HKObserverQuery(sampleType: sampleType, predicate: nil) { (query, completion, error) in
+    func startMonitoringHearthRate() {
+        let hearthRateObserverQuery = HKObserverQuery(sampleType: hearthRateType, predicate: nil) { (_, _, error) in
             if let error = error {
                 print("Error: \(error.localizedDescription)")
+                return
+            }
+
+            self.fetchLatestHeartRateSample { (sample) in
+                guard let sample = sample else {
+                    return
+                }
+
+                DispatchQueue.main.async {
+                    let heartRate = sample.quantity
+                    print("Heart Rate Sample: \(heartRate)")
+
+                    // do something with it
+                }
             }
         }
+
+        healthKitStore.execute(hearthRateObserverQuery)
+    }
+
+    func fetchLatestHeartRateSample(completionHandler: @escaping (_ sample: HKQuantitySample?) -> Void) {
+        guard let sampleType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate) else {
+            completionHandler(nil)
+            return
+        }
+
+        let predicate = HKQuery.predicateForSamples(withStart: Date.distantPast, end: Date(), options: .strictEndDate)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+        let query = HKSampleQuery(sampleType: sampleType,
+                                  predicate: predicate,
+                                  limit: Int(HKObjectQueryNoLimit),
+                                  sortDescriptors: [sortDescriptor]) { (_, results, error) in
+                                    if let error = error {
+                                        print("Error: \(error.localizedDescription)")
+                                        return
+                                    }
+
+                                    completionHandler(results?.first as? HKQuantitySample)
+        }
+
+        healthKitStore.execute(query)
     }
 }
 
@@ -51,7 +102,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func authorizeTapped(_ sender: Any) {
-        HealthKitManager.authorizeHealthKit()
+        manager.authorizeHealthKit()
     }
     
 }
